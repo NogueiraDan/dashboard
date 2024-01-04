b4129c511ab82690570149ae81bf20a81802adaa
NogueiraDan <silvanogueira3460@gmail.com>
2024-01-04 09:15:13 -0300

    feat: Cria testes automatizados

--- dashboard-api/__tests__/index.test.ts
+++ dashboard-api/__tests__/index.test.ts
@@ -1,5 +1,6 @@
 import { app } from "../src/server";
 import request from "supertest";
+
 beforeAll((done) => {
   done();
 });
@@ -12,5 +13,24 @@ describe("Endpoints test", () => {
   it("should get a list of users", async () => {
     const response = await request(app).get("/users");
     expect(response.status).toBe(200);
+  });
+
+  it("should return a login user", async () => {
+    const user = {
+      email: "usuario@demo.com",
+      password: "demo",
+    };
+    const response = await request(app).post("/login").send(user);
+    expect(response.status).toBe(200);
+  });
+
+  it("should return an error when user send invalids credentials", async () => {
+    const user = {
+      email: "usuario@demo.com",
+      password: "demowrong",
+    };
+    const response = await request(app).post("/login").send(user);
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty("error");
   });
 });
--- dashboard-api/jest.config.ts
+++ dashboard-api/jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -161,9 +161,7 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: ["\\\\node_modules\\\\", "\\\\dist\\\\"],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
